3054b9ee6baf8266680bf568f55e77c5f91c96be
vijaysaipranav <vijaysaypranavt@gmail.com>
2024-07-08 09:09:17 +0530

    added function to display the chart

--- coins/coins.js
+++ coins/coins.js
@@ -7,6 +7,7 @@ const coinDescription= document.getElementById("coin-description");
 const coinRank = document.getElementById("coin-rank");
 const coinPrice = document.getElementById("coin-price");
 const coinMarketCap = document.getElementById("coin-market-cap");
+const btnContainer = document.querySelectorAll('.button-container button') // targetting all the buttons inside the button container
 const ctx =document.getElementById("coinChart")
 
 const url = 'https://api.coingecko.com/api/v3/coins/';
@@ -57,11 +58,11 @@ const displayCoinsData = (coinsData)=>{
 const coinChart = new Chart(ctx,{
     type:"line",
     data:{
-        label:[],
+        label:[],// x axis
         datasets:[
             {
                 label:"Price (USD)",
-                data:[],
+                data:[], // y axis
                 borderWidth:1,
                 borderColor:"#eebc1d",
             }
@@ -76,7 +77,9 @@ const coinChart = new Chart(ctx,{
 const fetchChartData =async (days)=>{
     try {
         const response = await fetch(`${url}${coinId}/market_chart?vs_currency=usd&days=${days}`,options);
-        const chartData = response.json();
+        const chartData = await response.json();
+        // console.log(chartData.prices);
+        updateChart(chartData.prices)
 
     } catch (error) {
         console.log(error);
@@ -84,10 +87,33 @@ const fetchChartData =async (days)=>{
 }
 
 // Display the chart data
+const updateChart =(prices)=>{
+    // prices = [timestamp , price]
+    // y-axis :data => price, x-axis:labels => timestamp
+    const data = prices?.map((price)=>price[1])
+    const labels = prices?.map((price)=> {
+        let date =new Date(price[0]).toLocaleDateString()
+        return date;
+    });
+    // console.log(data);
+    // console.log(labels);
+    coinChart.data.labels=labels;
+    coinChart.data.datasets[0].data =data;
+    coinChart.update();
+}
 
+// on btn click fetch the chart data and display it
+btnContainer.forEach((btn)=>{
+    btn.addEventListener("click",(event)=>{
+        const days = event.target.id ==="24hr" ? 1 : event.target.id === "30d" ? 30 : 90;
+        fetchChartData(days);
+    })
+})
 
 document.addEventListener('DOMContentLoaded',async()=>{
     showShimmer()
     await fetchCoinsData(coinId);
+    // set 24h as default
+    document.getElementById("24hr").click()
     hideShimmer()
 })
